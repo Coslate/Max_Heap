166da73a2de0a821ec3dbdb07b304c53d57e08b0
Coslate <coslate@media.ee.ntu.edu.tw>
2021-02-12 22:59:31 +0800

    Update MaxHeap.

--- Heap_Sort_main.cpp
+++ Heap_Sort_main.cpp
@@ -26,59 +26,6 @@ void MoveArray(const int* from_arr, int* to_arr, const int to_size, const int to
     }
 }
 
-void HeapSort_V01::SiftDown(Heap &H, const int index){
-    int parent             = index;
-    int sift_key           = H.get_heap_index(parent);
-    int heap_size          = H.get_heap_size();
-    int larger_child_index = 0;
-    bool spot_found        = false;
-
-    while(((parent*2+1) < heap_size) && (!spot_found)){
-        if((parent*2+1) == (heap_size-1)){
-            larger_child_index = parent*2 + 1;
-        }else if ((parent*2+1) < (heap_size-1)){
-            if(H.get_heap_index(parent*2+1) < H.get_heap_index(parent*2+2)){
-                larger_child_index = parent*2+2;
-            }else{
-                larger_child_index = parent*2+1;
-            }
-        }
-        if(sift_key < H.get_heap_index(larger_child_index)){
-            H.set_heap_index(parent, H.get_heap_index(larger_child_index));
-            parent = larger_child_index;
-        }else{
-            spot_found = true;
-        }
-    }
-    H.set_heap_index(parent, sift_key);
-}
-
-void HeapSort_V01::MakeHeap(const int size, Heap &H){
-    for(int i = (floor(size/2)-1); i>=0; --i){
-        SiftDown(H, i);
-    }
-}
-
-int HeapSort_V01::Root(Heap &H){
-    int root_key = H.get_heap_root();
-    H.set_heap_root(H.get_heap_tail());
-    H.set_heap_size(H.get_heap_size() - 1);
-    SiftDown(H, 0);
-    return root_key;
-}
-
-void HeapSort_V01::RemoveKey(const int size, Heap &H){
-    for(int i = size-1;i>=0;--i){
-        H.set_heap_index(i, Root(H));
-    }
-    H.set_heap_size(size);
-}
-
-void HeapSort_V01::HeapSort(const int size, Heap &H){
-    MakeHeap(size, H);
-    RemoveKey(size, H);
-}
-
 int main(){
     const int n = 10;
     Heap H(n);
